19734993d0e13733862c9e09b472dae373ca331c
Thiago Granata <granatathiago@gmail.com>
2023-02-09 12:05:33 -0300

    feat: chat screen

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { ChatCard } from '../../components/ChatCard';
 
 const ChatPage = () => {
   return (
-    <div>ChatPage</div>
+    <section className='bg-primary h-screen'>
+        <div className='bg-primary w-full h-32 grid content-center p-6'>
+          <input type="text" placeholder='Search Trainers...' className='bg-gray-100 p-1 rounded-lg w-full'/>
+        </div>
+
+        <div className="rounded-t-lg h-full bg-white p-6 flex flex-col gap-4">
+          <ChatCard />
+        </div>
+    </section>
   )
 }
 
