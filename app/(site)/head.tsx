58f452af7a2a79b07cd2a5320da4e3fdd2ce5f41
Thiago Granata <granatathiago@gmail.com>
2023-02-14 13:08:07 -0300

    feat: added seo meta tags to BaseHead

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -1,9 +1,30 @@
 export default function Head() {
     return (
       <>
-        <title></title>
+        <title>Communifit</title>
+        <meta name="title" content='Communifit' />
+        <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1"  />
         <link rel="icon" href="/favicon.ico" />        
+        <meta name="description" content="Communifit is an application where Personal Trainers create communities for all of their clients!"/>        
+        <meta name="keywords" content="trainer, members, community, workout, weights, shred, bulk, dieting, diet" />
+        <meta name="author" content="Unslided" />
+        <meta name="copyright" content="Unslided 2023" />
+
+        {/* <!-- Facebook --> */}
+        <meta property="og:title" content='Communifit' />
+        <meta property="og:description" content='Start training like you should' />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content='' />
+        <meta property="og:url" content='' />
+
+        {/* <!-- Twitter --> */}
+        <meta name="twitter:creator" content="@communifit" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content='Communifit' />
+        <meta name="twitter:description" content='' />
+        <meta name="twitter:image" content='' />
+        <meta name="twitter:url" content='' />
       </>
     );
   }
