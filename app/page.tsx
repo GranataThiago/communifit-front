54e86c5caed53a987557f1684ab7cd1b99145205
Tomás Hernández <hernandeztomas584@gmail.com>
2023-07-08 19:22:35 -0300

    feat: first commit testing inline styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
     return (
    
-        <h1 className="text-red-500">Hello World</h1>
+        <h1 className="text-red-500">Testing inline styles</h1>
  
     )
   }
