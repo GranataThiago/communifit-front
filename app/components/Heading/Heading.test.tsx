931bbdcdb06ca7730e4027c63b154cd7dd398666
joakom24 <joakoomussi@gmail.com>
2023-10-09 17:48:13 -0300

    Modify heading and create test for component

---
+++ app/components/Heading/Heading.test.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import Heading from "./Heading";
+
+afterAll(() => {
+	cleanup();
+	jest.clearAllMocks();
+});
+
+describe("<Heading />", () => {
+	it("should render a Heading element with title, subtitle and center", () => {
+		const title = "Hello I'm Developer";
+		const subtitle = "Bye";
+		const { getByText, container } = render(
+			<Heading center={true} subtitle={subtitle} title={title} />
+		);
+		expect(getByText(title)).toBeInTheDocument();
+		expect(getByText(subtitle)).toBeInTheDocument();
+		expect(container.firstChild).toHaveClass("text-center");
+	});
+
+	it("should render a Heading element with title, subtitle and withouth center", () => {
+		const title = "Hello I'm Developer";
+		const subtitle = "Bye";
+		const { getByText, container } = render(
+			<Heading center={false} subtitle={subtitle} title={title} />
+		);
+		expect(getByText(title)).toBeInTheDocument();
+		expect(getByText(subtitle)).toBeInTheDocument();
+		expect(container.firstChild).toHaveClass("text-start");
+	});
+});
