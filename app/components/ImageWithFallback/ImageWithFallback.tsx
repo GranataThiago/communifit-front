930c3c3421abb762bddbf6b08f1f34f97c90e643
joakom24 <joakoomussi@gmail.com>
2023-10-09 19:12:21 -0300

    create test for ImageWithFallback component

--- app/components/ImageWithFallback/ImageWithFallback.tsx
+++ app/components/ImageWithFallback/ImageWithFallback.tsx
@@ -1,11 +1,11 @@
 import { useState, useEffect, FC, SyntheticEvent } from 'react';
 import Image, { ImageProps } from 'next/image';
 
-import fallbackImage from '../../public/vercel.svg';
 
 interface ImageWithFallbackProps extends ImageProps {
     fallback?: ImageProps['src']
 }
+const fallbackImage = "public/vercel.svg";
 
 export const ImageWithFallback: FC<ImageWithFallbackProps> = ({
     fallback = fallbackImage,
