e51162555bfe64ac95d95ffb1383c01d27095a4b
joakom24 <joakoomussi@gmail.com>
2023-10-09 17:36:31 -0300

    Add tests for button component

---
+++ app/components/Button/Button.test.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import { Button } from "./Button";
+
+afterAll(() => {
+	cleanup();
+	jest.clearAllMocks();
+});
+
+describe("<Button />", () => {
+	it("should render a button element with children and variant props", () => {
+		const variant = "filled";
+		const children = "Click me";
+		const { container } = render(<Button variant={variant}>{children}</Button>);
+
+		expect(container.firstChild).toBeInTheDocument();
+		expect(container.firstChild).toHaveClass(
+			"bg-primary text-white rounded-full w-full py-3 font-[500] disabled:bg-gray-500"
+		);
+		expect(container.firstChild).toHaveTextContent(children);
+	});
+
+	it("should render a button element with additional className prop", () => {
+		const className = "custom-class";
+		const variant = "filled";
+		const { container } = render(
+			<Button variant={variant} className={className}>
+				Click me
+			</Button>
+		);
+
+		expect(container.firstChild).toBeInTheDocument();
+		expect(container.firstChild).toHaveClass(className);
+	});
+});
