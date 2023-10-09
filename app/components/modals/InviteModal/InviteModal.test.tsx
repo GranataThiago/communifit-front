dbcb3e475dfd8294aa3f69e5bb87348af03327ff
joakom24 <joakoomussi@gmail.com>
2023-10-09 19:56:14 -0300

    create temporally test for Modals

--- app/components/modals/InviteModal/InviteModal.test.tsx
+++ app/components/modals/InviteModal/InviteModal.test.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { render, fireEvent, waitFor } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import InviteModal from "./InviteModal";
-import useInviteModal from "../../../hooks/modals/useInviteModal";
-
-// Mockear el hook useInviteModal
-jest.mock("../../../hooks/modals/useInviteModal");
 
 describe("<InviteModal />", () => {
-	// add test for component
+	it("render component", () => {
+		render(<InviteModal />);
+	});
 });
--- app/components/modals/WorkoutModal/WorkoutModal.test.tsx
+++ app/components/modals/WorkoutModal/WorkoutModal.test.tsx
@@ -1,22 +1,9 @@
 import React from "react";
-import { render, fireEvent, waitFor } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import WorkoutModal from "./WorkoutModal";
 
-// Importa el módulo real de useWorkoutModal
-import useWorkoutModal from "../../../hooks/modals/useWorkoutModal";
-
-// Mockea el módulo de useWorkoutModal
-jest.mock("../../../hooks/modals/useWorkoutModal", () => ({
-	__esModule: true,
-	default: jest.fn(),
-	isOpen: true,
-	onOpen: jest.fn(),
-	onClose: jest.fn(),
-	exercise: null,
-}));
-
 describe("<WorkoutModal />", () => {
-	it("renders the modal and submits the form", async () => {
-		// create test for workout modal
+	it("renders component", () => {
+		render(<WorkoutModal />);
 	});
 });
