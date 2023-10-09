d8e5bf08dc3f52f8ea53a01e8a425c039f268547
joakom24 <joakoomussi@gmail.com>
2023-10-09 17:03:17 -0300

    modify tsconfig and delete test

--- app/layout.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen, cleanup } from "@testing-library/react";
-
-afterAll(() => {
-	cleanup;
-	jest.clearAllMocks();
-});
-
-describe("<Layout />", () => {});
