70d07ca3466aad092a3059056c422027f35f351a
joakom24 <joakoomussi@gmail.com>
2023-10-09 17:03:06 -0300

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,24 @@
 module.exports = {
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    moduleDirectories: ['node_modules', '<rootDir>/'],
+    testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+    testEnvironment: 'jsdom',
+    clearMocks: true,
     collectCoverage: false,
-    collectCoverageFrom: ["app/**/*.tsx"],
+    collectCoverageFrom: [ 
+    '<rootDir>/app/components/**',
+    '<rootDir>/app/**.tsx',
+    '!<rootDir>/node_modules/**',
+    '!<rootDir>/.next/**',
+    ],
     coverageDirectory: "coverage",
-    testEnvironment: "jsdom",
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
-}
+    preset: 'ts-jest',
+    coverageThreshold: {
+        global: {
+            branches: 75,
+            functions: 75,
+            lines: 80,
+            statements: 80,
+        },
+    },
+}    
