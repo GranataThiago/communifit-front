2406793774bd2af5f7868f168a2fa23a9935fb12
joakom24 <joakoomussi@gmail.com>
2023-10-10 11:40:22 -0300

    fix types into useEffect

--- app/(site)/components/Workout.tsx
+++ app/(site)/components/Workout.tsx
@@ -1,75 +1,106 @@
 "use client"
-import React, { LiHTMLAttributes, useEffect, useReducer, useState } from 'react'
-import { montserrat } from '../../components/fonts'
-import { Button } from "../../components/Button/Button";
-import useWorkoutModal from '../../hooks/modals/useWorkoutModal';
-import { useUserContext } from '../../../context/UserContext';
+import React, {
+	LiHTMLAttributes,
+	useEffect,
+	useReducer,
+	useState,
+} from "react";
+import useWorkoutModal from "../../hooks/modals/useWorkoutModal";
+import { useUserContext } from "../../../context/UserContext";
+import { Button, montserrat } from "../../components";
 
-type WorkoutState = {[day: string]: Exercise[]};
+type WorkoutState = { [day: string]: Exercise[] };
 
 const WEEK_DAYS = [
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-    'Sunday'
-]
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday",
+	"Sunday",
+];
 
-export const Workout = () => {    
-    const [day, setDay] = useState('Monday');
-    const [exercises, setExercises] = useState<WorkoutState>({
-        'Monday': [],
-        'Tuesday': [],
-        'Wednesday': [],
-        'Thursday': [],
-        'Friday': [],
-        'Saturday': [],
-        'Sunday': [],
-    });
+export const Workout = () => {
+	const [day, setDay] = useState("Monday");
+	const [exercises, setExercises] = useState<WorkoutState>({
+		Monday: [],
+		Tuesday: [],
+		Wednesday: [],
+		Thursday: [],
+		Friday: [],
+		Saturday: [],
+		Sunday: [],
+	});
 
-    const { user } = useUserContext();
-    const workoutModal = useWorkoutModal();
+	const { user } = useUserContext();
+	const workoutModal = useWorkoutModal();
 
-    useEffect(() => {
-        if(!workoutModal.exercise) return;
-        setExercises(prevState => ({
-            ...prevState,
-            [day]: [...prevState[day], workoutModal.exercise]
-        }))
-    }, [workoutModal.exercise])
+	useEffect(() => {
+		if (!workoutModal.exercise) return;
 
-    const onDayChanged = (selectedDay: string) => {
-        setDay(selectedDay);
-    }
+		// Copy of state
+		const updatedExercises = { ...exercises };
 
-    return (
-        <section className={`workout flex flex-col gap-2 pb-6 ${montserrat.className}`}>
-            <ul className="flex gap-3 overflow-x-scroll no-scrollbar">
-                {
-                    WEEK_DAYS.map((weekDay, index) => (
-                        <li key={index} onClick={() => onDayChanged(weekDay)} className={`font-normal ${day === weekDay ? 'text-black' : 'text-gray-300'}`}>{weekDay}</li>
-                    ))
-                }
-            </ul>
+		// Get actually day
+		const currentDayExercises = updatedExercises[day];
 
-            {user?.type == 'trainer' && <Button variant='filled' className='py-2 w-1/3 ml-auto' onClick={workoutModal.onOpen}>Add</Button> /* TODO: Check if its owner or authorized trainer from community */ } 
+		// Add exercise into now
+		updatedExercises[day] = [...currentDayExercises, workoutModal.exercise];
 
-            <ul>
-                {
-                    exercises[day].map((ex, index) => (
-                        <li key={index} className="border border-gray-300 rounded-md p-2 flex justify-between items-center">
-                            <p className="flex flex-col">
-                                {ex.name}
-                                <span>{ex.quantity}</span>
-                            </p>
+		// Update state
+		setExercises(updatedExercises);
+	}, [workoutModal.exercise]);
 
-                            <p>{ex.weight}</p>
-                        </li>
-                    ))
-                }
-            </ul>
-        </section>
-    )
-}
+	const onDayChanged = (selectedDay: string) => {
+		setDay(selectedDay);
+	};
+
+	return (
+		<section
+			className={`workout flex flex-col gap-2 pb-6 ${montserrat.className}`}
+		>
+			<ul className='flex gap-3 overflow-x-scroll no-scrollbar'>
+				{WEEK_DAYS.map((weekDay, index) => (
+					<li
+						key={index}
+						onClick={() => onDayChanged(weekDay)}
+						className={`font-normal ${
+							day === weekDay ? "text-black" : "text-gray-300"
+						}`}
+					>
+						{weekDay}
+					</li>
+				))}
+			</ul>
+
+			{
+				user?.type == "trainer" && (
+					<Button
+						variant='filled'
+						className='py-2 w-1/3 ml-auto'
+						onClick={workoutModal.onOpen}
+					>
+						Add
+					</Button>
+				) /* TODO: Check if its owner or authorized trainer from community */
+			}
+
+			<ul>
+				{exercises[day].map((ex, index) => (
+					<li
+						key={index}
+						className='border border-gray-300 rounded-md p-2 flex justify-between items-center'
+					>
+						<p className='flex flex-col'>
+							{ex.name}
+							<span>{ex.quantity}</span>
+						</p>
+
+						<p>{ex.weight}</p>
+					</li>
+				))}
+			</ul>
+		</section>
+	);
+};
