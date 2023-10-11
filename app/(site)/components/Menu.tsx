10c815bf37bc15da62ecfc836bb8a749d85d8c4c
joakom24 <joakoomussi@gmail.com>
2023-10-11 18:52:05 -0300

    add test for menu

--- app/(site)/components/Menu.tsx
+++ app/(site)/components/Menu.tsx
@@ -11,29 +11,54 @@ export const Menu = () => {
     const { user } = useUserContext();
 
     return (
-        <nav className='w-full py-4 px-6 fixed bottom-0 right-0 bg-white z-50'>
-            <ul className='w-full flex justify-between text-xl'>
-                <li className={`hover:text-gray-700 hover:cursor-pointer ${(pathname === '/' ? 'text-primary' : 'text-black')}`}>
-                    <Link href={'/'}>
-                        <BsHouseDoor></BsHouseDoor>
-                    </Link>
-                </li>
-                <li className={`hover:text-gray-700 hover:cursor-pointer ${(pathname === '/chat' ? 'text-primary' : 'text-black')}`}>
-                    <Link href={'/chat'}>
-                        <BsChat></BsChat>
-                    </Link>
-                </li>
-                <li className={`hover:text-gray-700 hover:cursor-pointer ${(pathname === '/community' ? 'text-primary' : 'text-black')}`}>
-                    <Link href={user?.community?.name ? `/community/${user.community.name}` : '/community'}>
-                        {user?.community?.name ? <BsPeople /> : <BsSearch />}
-                    </Link>
-                </li>
-                <li className={`hover:text-gray-700 hover:cursor-pointer ${(pathname === '/profile' ? 'text-primary' : 'text-black')}`}>
-                    <Link href={'/profile'}>
-                        <BsPerson></BsPerson>
-                    </Link>
-                </li>
-            </ul>
-        </nav>
-    )
+			<nav
+				className='w-full py-4 px-6 fixed bottom-0 right-0 bg-white z-50'
+				data-testid='nav'
+			>
+				<ul className='w-full flex justify-between text-xl' data-testid='ul'>
+					<li
+						className={`hover:text-gray-700 hover:cursor-pointer ${
+							pathname === "/" ? "text-primary" : "text-black"
+						}`}
+					>
+						<Link href={"/"}>
+							<BsHouseDoor></BsHouseDoor>
+						</Link>
+					</li>
+					<li
+						className={`hover:text-gray-700 hover:cursor-pointer ${
+							pathname === "/chat" ? "text-primary" : "text-black"
+						}`}
+					>
+						<Link href={"/chat"}>
+							<BsChat></BsChat>
+						</Link>
+					</li>
+					<li
+						className={`hover:text-gray-700 hover:cursor-pointer ${
+							pathname === "/community" ? "text-primary" : "text-black"
+						}`}
+					>
+						<Link
+							href={
+								user?.community?.name
+									? `/community/${user.community.name}`
+									: "/community"
+							}
+						>
+							{user?.community?.name ? <BsPeople /> : <BsSearch />}
+						</Link>
+					</li>
+					<li
+						className={`hover:text-gray-700 hover:cursor-pointer ${
+							pathname === "/profile" ? "text-primary" : "text-black"
+						}`}
+					>
+						<Link href={"/profile"}>
+							<BsPerson></BsPerson>
+						</Link>
+					</li>
+				</ul>
+			</nav>
+		);
 }
