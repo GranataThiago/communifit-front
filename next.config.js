2125d7b5c865822df28c4a905669d565b96c63e4
joakom24 <joakoomussi@gmail.com>
2023-10-11 16:08:03 -0300

    delete ignoreTypescript into NextConfig

--- next.config.js
+++ next.config.js
@@ -5,9 +5,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  typescript:{
-    ignoreBuildErrors: true,
-  },
   images: {
     domains: ["pbs.twimg.com"],
     remotePatterns: [
