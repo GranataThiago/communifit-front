f803abe8cb5f29f5fd313493844003a6597be33d
joakom24 <joakoomussi@gmail.com>
2023-10-11 19:44:02 -0300

    add workout test

---
+++ app/(site)/components/Workout/Workout.test.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { fireEvent, render } from "@testing-library/react";
+import { Workout } from "./Workout";
+jest.mock("../../../../context/UserContext", () => ({
+	useUserContext: () => ({
+		user: {
+			fullname: "John Doe",
+			username: "johndoe",
+		},
+		logout: jest.fn(),
+	}),
+}));
+describe("<Workout />", () => {
+	it("renders Workout component", () => {
+		const { getByText } = render(<Workout />);
+		expect(getByText("Monday")).toBeInTheDocument();
+		expect(getByText("Tuesday")).toBeInTheDocument();
+	});
+	it("switches the active day on click", () => {
+		const { getByText } = render(<Workout />);
+		const tuesdayButton = getByText("Tuesday");
+
+		fireEvent.click(tuesdayButton);
+		expect(tuesdayButton).toHaveClass("text-black");
+
+		const activeDay = getByText("Monday");
+		expect(activeDay).toHaveClass("text-gray-300");
+	});
+});
