7ed931dbb520949ce143eb355936e47be07d87a0
joakom24 <joakoomussi@gmail.com>
2023-10-12 12:52:49 -0300

    fix some test

--- app/(site)/community/[name]/page.test.tsx
+++ app/(site)/community/[name]/page.test.tsx
@@ -1,31 +1,42 @@
 import React from "react";
-import { render, cleanup } from "@testing-library/react";
-import CommunityPage from "./page";
+import { render, cleanup, waitFor } from "@testing-library/react";
+import Page from "./page";
+import MockAdapter from "axios-mock-adapter";
+import axios from "axios";
 
 afterAll(() => {
 	cleanup();
 	jest.clearAllMocks();
 });
 
-jest.mock("next/headers", () => {
-	return {
+// Simula Axios con axios-mock-adapter
+const mock = new MockAdapter(axios);
+
+// Configura una respuesta simulada
+const mockResponse = {
+	id: 1,
+	name: "John Doe",
+};
+
+mock.onGet("/communities/gorillas/posts?page=1").reply(200, mockResponse);
+
+jest.mock("next/headers", () => ({
+	cookies: jest.fn().mockReturnValue({
+		get: jest.fn().mockReturnValue({ token: "tuToken" }),
+	}),
+}));
+
+jest.mock("../../../api", () => ({
+	default: {
 		get: jest.fn(),
-	};
-});
+	},
+}));
 
 describe("<Page />", () => {
 	it("should render CommunityScreen component when community data is available", async () => {
-		const mockGetCommunity = jest.fn().mockResolvedValue({
-			name: "Test Community",
-			description: "Test Description",
-			posts: [],
-		});
-
-		const { getByText } = render(await CommunityPage({}));
+		const { getByText } = render(await Page({}));
 
 		expect(getByText("Test Community")).toBeInTheDocument();
 		expect(getByText("Test Description")).toBeInTheDocument();
-
-		expect(mockGetCommunity).toHaveBeenCalled();
 	});
 });
