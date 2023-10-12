9980f60187d9f6e98fd99aaaec7a70f6f642201a
joakom24 <joakoomussi@gmail.com>
2023-10-12 09:23:16 -0300

    add page test community

---
+++ app/(site)/community/page.test.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import Page from "./page";
+
+afterAll(() => {
+	cleanup();
+	jest.clearAllMocks();
+});
+
+describe("<Page />", () => {
+	it("renders CommunityPage", async () => {
+		const { getByText } = render(await Page());
+		expect(getByText("Community"));
+	});
+});
