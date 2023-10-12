5a5c18891c323c9533efa4d9901ecd9109c27a39
Tomás Hernández <thernandez@ptn.gob.ar>
2023-10-12 11:17:53 -0300

    add: bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,38 +1,45 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    appDir: true,
-  },
-  images: {
-    domains: ["pbs.twimg.com"],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'i.pravatar.cc',
-      }
-     
-    ]
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/",
-        destination: "/site"
-      },
-    ];
-  },
-  async redirects() {
-    return [
-      {
-        source: "/site",
-        destination: "/",
-        permanent: true
-      }
-    ]
-  }
 
-}
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+const nextConfig = withBundleAnalyzer(
+  {
+    reactStrictMode: true,
+    swcMinify: true,
+    experimental: {
+      appDir: true,
+    },
+    images: {
+      domains: ["pbs.twimg.com"],
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'i.pravatar.cc',
+        }
+       
+      ]
+    },
+    async rewrites() {
+      return [
+        {
+          source: "/",
+          destination: "/site"
+        },
+      ];
+    },
+    async redirects() {
+      return [
+        {
+          source: "/site",
+          destination: "/",
+          permanent: true
+        }
+      ]
+    }
+  
+  }
+)
 
 module.exports = nextConfig
