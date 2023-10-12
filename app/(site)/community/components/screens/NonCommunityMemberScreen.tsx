f67e98efcdcf21c498fe5caa3a7f9ecca66b0b39
joakom24 <joakoomussi@gmail.com>
2023-10-12 14:42:33 -0300

    add missing tests

--- app/(site)/community/components/screens/NonCommunityMemberScreen.tsx
+++ app/(site)/community/components/screens/NonCommunityMemberScreen.tsx
@@ -30,19 +30,16 @@ export const NonCommunityMemberScreen = async() => {
   const communities = await getCommunities();
 
   return (
-    <section className='p-6'>
-        <Input variant='filled' type="text" placeholder='Search Trainers...'  />
+		<section className='p-6' data-testid='section'>
+			<Input variant='filled' type='text' placeholder='Search Trainers...' />
 
-        <p className='mt-6 text-3xl font-bold'>Recommended for you</p>
+			<p className='mt-6 text-3xl font-bold'>Recommended for you</p>
 
-        <div className="flex flex-col gap-6 mt-6">
-          {
-            communities.map(community => (
-              <CommunityCard key={community.name} {...community} />
-            ))
-          }
-
-        </div>
-    </section>
-  )
+			<div className='flex flex-col gap-6 mt-6'>
+				{communities.map((community) => (
+					<CommunityCard key={community.name} {...community} />
+				))}
+			</div>
+		</section>
+	);
 }
