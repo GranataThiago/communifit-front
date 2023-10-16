364362ee0d17864282c1e21d04a200090d79552e
mussiDev <joakoomussi@gmail.com>
2023-10-15 21:01:56 -0300

    modify filled style for login

--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -11,8 +11,7 @@ type ButtonVariant = "outlined" | "filled" | "text";
 type ButtonStyles = { [key: string]: string };
 
 const buttonVariants: ButtonStyles = {
-	filled:
-		"bg-primary text-white rounded-full w-full py-3 font-[500] disabled:bg-gray-500",
+	filled: "bg-primary text-white h-[3.25rem] rounded-[1.6875rem] w-full",
 	outlined:
 		"border border-primary text-white rounded-full w-full py-3 font-[500] disabled:border-gray-500",
 	text: "bg-transparent text-primary rounded-full w-full py-3 font-[500] disabled:text-gray-500",
