dac54b5fd60223fba8314221e76d8acf75d638fc
mussiDev <joakoomussi@gmail.com>
2023-10-16 18:32:19 -0300

    add styles for more accesibility buttons

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "../../lib/utils";
 
 const buttonVariants = cva(
-	"inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+	"inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 cursor-pointer hover:brightness-105 tranisiton all",
 	{
 		variants: {
 			variant: {
@@ -37,6 +37,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 		const Comp = asChild ? Slot : "button";
 		return (
 			<Comp
+				role='button'
 				className={cn(buttonVariants({ variant, size, className }))}
 				ref={ref}
 				disabled={canSubmit}
