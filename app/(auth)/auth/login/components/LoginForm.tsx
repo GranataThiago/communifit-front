9f7927e798f2786a4e1a683ff743ecad9ac4cbc9
mussiDev <joakoomussi@gmail.com>
2023-10-16 18:51:59 -0300

    add component for more atomic

--- app/(auth)/auth/login/components/LoginForm.tsx
+++ app/(auth)/auth/login/components/LoginForm.tsx
@@ -12,8 +12,10 @@ import {
 import React, { useState } from "react";
 
 import { Button } from "../../../../components/ui/button";
+import ForgotPasswordLink from "./ForgotPasswordLink/ForgotPasswordLink";
 import { Input } from "../../../../components/ui/input";
 import Link from "next/link";
+import LoginFormFields from "./LoginFormFields/LoginFormFields";
 import { LoginUserResponse } from "../../../../../interfaces";
 import { montserrat } from "../../../../components/fonts";
 import { useForm } from "react-hook-form";
@@ -78,56 +80,9 @@ export const LoginForm = () => {
 
 	return (
 		<Form {...form}>
-			<form
-				className={`w-full flex flex-col gap-4 pt-2 ${montserrat.className}`}
-				onSubmit={form.handleSubmit(onLogin)}
-			>
-				<FormField
-					rules={{
-						required: "The email is required.",
-					}}
-					control={form.control}
-					name='email'
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Your email adress</FormLabel>
-							<FormControl>
-								<Input
-									{...field}
-									placeholder='Mail@example.com'
-									variant='outlined'
-									type='email'
-								/>
-							</FormControl>
-						</FormItem>
-					)}
-				/>
-				<FormField
-					control={form.control}
-					name='password'
-					rules={{
-						required: "The password is required.",
-					}}
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Password</FormLabel>
-							<FormControl>
-								<Input
-									{...field}
-									placeholder='Enter your password...'
-									variant='outlined'
-									type='password'
-								/>
-							</FormControl>
-						</FormItem>
-					)}
-				/>
-				<Link
-					className='text-right text-sm font-normal tracking-[-0.0255rem] leading-[1.375rem]'
-					href='/auth/forgot-password'
-				>
-					Forgot password?
-				</Link>
+			<form className={`w-full flex flex-col gap-4 pt-2`}>
+				<LoginFormFields form={form} />
+				<ForgotPasswordLink />
 				{messageError != "" && <p className='text-red-500'>{messageError}</p>}
 				<Button variant='filled' type='submit' aria-label='Continue with login'>
 					Continue
