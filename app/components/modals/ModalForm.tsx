0e30812f39ee396d4632e169e4604b5e1143ced9
mussiDev <joakoomussi@gmail.com>
2023-10-16 21:48:15 -0300

    Refactor modalForm ,change styles and modify value input

--- app/components/modals/ModalForm.tsx
+++ app/components/modals/ModalForm.tsx
@@ -33,6 +33,7 @@ interface InputProps {
 	className?: string;
 	type: string;
 	fieldName: string;
+	value?: string;
 }
 
 interface ModalProps {
@@ -43,60 +44,67 @@ interface ModalProps {
 	Labels?: LabelProps[];
 	OnSubmit?: () => void;
 	Form?: any;
+	Icon?: any;
 }
 
 const ModalForm = (props: ModalProps) => {
-	const { TextButton, Footer, Description, Title, Labels, OnSubmit, Form } =
-		props;
+	const {
+		TextButton,
+		Footer,
+		Description,
+		Title,
+		Labels,
+		OnSubmit,
+		Form,
+		Icon,
+	} = props;
 
 	return (
 		<Dialog>
 			<DialogTrigger asChild>
-				<Button variant='outlined' className='bg-primary text-white'>
-					{TextButton}
-				</Button>
+				<Button variant='filled'>{TextButton}</Button>
 			</DialogTrigger>
 			<DialogContent className='sm:max-w-[425px]'>
-				<DialogHeader>
+				<DialogHeader className='gap-4'>
 					<DialogTitle>{Title}</DialogTitle>
 					<DialogDescription>{Description}</DialogDescription>
 				</DialogHeader>
-				<div className='grid gap-4 py-4'>
-					<form className='grid grid-cols-4 items-center gap-4'>
-						{Labels?.map((label: LabelProps, key: number) => (
-							<FormField
-								control={Form.control}
-								name={label.input.fieldName}
-								key={key}
-								render={({ field }) => (
-									<FormItem>
-										<FormLabel
-											htmlFor={label.htmlFor}
-											className={label.className}
-										>
-											{label.label}
-										</FormLabel>
-										<FormControl>
-											<Input
-												{...field}
-												id={label.input.id}
-												variant={label.input.variant}
-												className={label.input.className}
-												type={label.input.type}
-												ref={null}
-											/>
-										</FormControl>
-										<FormMessage />
-									</FormItem>
-								)}
-							/>
-						))}
-					</form>
-				</div>
+				<form className='flex flex-col w-full gap-4'>
+					{Labels?.map((label: LabelProps, key: number) => (
+						<FormField
+							control={Form.control}
+							name={label.input.fieldName}
+							key={key}
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel
+										htmlFor={label.htmlFor}
+										className={label.className}
+									>
+										{label.label}
+									</FormLabel>
+									<FormControl>
+										<Input
+											{...field}
+											id={label.input.id}
+											variant={label.input.variant}
+											className={label.input.className}
+											type={label.input.type}
+											ref={null}
+											value={label.input.value}
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+					))}
+				</form>
 
 				<DialogFooter>
-					<Button type='submit' onSubmit={OnSubmit}>
+					<Button type='submit' onClick={OnSubmit} className='flex gap-2'>
 						{Footer}
+						{Icon}
 					</Button>
 				</DialogFooter>
 			</DialogContent>
