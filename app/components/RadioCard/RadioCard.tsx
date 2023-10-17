bd3af391122acc9e3b9a07a99493ffbe18d656ce
mussiDev <joakoomussi@gmail.com>
2023-10-17 12:20:34 -0300

    migrate radio card to new component card

--- app/components/RadioCard/RadioCard.tsx
+++ app/components/RadioCard/RadioCard.tsx
@@ -1,29 +1,41 @@
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import React, { InputHTMLAttributes, ReactNode } from "react";
 
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+
 interface RadioCardProps extends InputHTMLAttributes<HTMLInputElement> {
-  label: string;
-  height?: number;
-  icon?: ReactNode;
-  ref: any;
+	label: string;
+	height?: number;
+	icon?: ReactNode;
+	ref: React.Ref<any>;
 }
 
 export const RadioCard = ({
-  label,
-  children,
-  icon,
-  height,
-  ...props
+	label,
+	children,
+	icon,
+	height,
+	...props
 }: RadioCardProps) => {
-  return (
-    <label
-      className={`border border-gray-400 w-full flex flex-col items-center justify-center relative rounded-lg h-${height}`}
-      htmlFor={props.id}
-    >
-      {icon ? icon : null}
-      <input className="absolute top-2 right-2" type="radio" {...props} />
-      <div className="w-4/5 font-semibold text-xl text-center">{label}</div>
-    </label>
-  );
+	return (
+		<Card
+			className={`flex p-4 w-full items-center justify-center rounded-lg h-${height}`}
+		>
+			<CardTitle className='w-full text-center'>
+				<Label
+					htmlFor={props.id}
+					className='w-4/5 font-semibold xxs:text-sm xs:text-lg'
+				>
+					{label}
+				</Label>
+			</CardTitle>
+			<CardContent>
+				{icon ? icon : null}
+				<Input variant='outlined' type='radio' {...props} />
+			</CardContent>
+		</Card>
+	);
 };
 
 const Icon = (icon: React.ReactNode) => <>{icon}</>;
