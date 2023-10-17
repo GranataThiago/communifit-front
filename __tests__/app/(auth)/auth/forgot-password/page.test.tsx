048792cae15db9eefe0b00a4566e29ec96d43d14
Thiago Granata <granatathiago@gmail.com>
2023-10-17 13:02:47 -0300

    fix: removed unnecessary mocking data

--- __tests__/app/(auth)/auth/forgot-password/page.test.tsx
+++ __tests__/app/(auth)/auth/forgot-password/page.test.tsx
@@ -10,18 +10,7 @@ afterAll(() => {
 jest.mock("next/navigation", () => ({
   useRouter() {
     return {
-      route: "/",
-      pathname: "",
-      query: "",
-      asPath: "",
       push: jest.fn(),
-      replace: jest.fn(),
-      events: {
-        on: jest.fn(),
-        off: jest.fn(),
-      },
-      beforePopState: jest.fn(() => null),
-      prefetch: jest.fn(() => null),
     };
   },
 }));
