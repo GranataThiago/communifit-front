c5b9b168eafe0cf3e8c1f8f80d073cb28ccda8b5
mussiDev <joakoomussi@gmail.com>
2023-10-16 21:07:05 -0300

    fix form

--- app/components/ui/form.tsx
+++ app/components/ui/form.tsx
@@ -7,7 +7,7 @@ import {
 	FieldPath,
 	FieldValues,
 	FormProvider,
-	useFormContext,
+	useForm,
 } from "react-hook-form";
 
 import { Label } from "./label";
@@ -43,7 +43,7 @@ const FormField = <
 const useFormField = () => {
 	const fieldContext = React.useContext(FormFieldContext);
 	const itemContext = React.useContext(FormItemContext);
-	const { getFieldState, formState } = useFormContext();
+	const { getFieldState, formState } = useForm();
 
 	const fieldState = getFieldState(fieldContext.name, formState);
 
