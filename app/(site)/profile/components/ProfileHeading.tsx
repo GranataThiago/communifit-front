8588c0c9ddca07b16292403021561d51fb4a0b4b
tmgranata <granatathiago@gmail.com>
2024-01-03 16:09:38 -0300

    feat: rebranded profile

--- app/(site)/profile/components/ProfileHeading.tsx
+++ app/(site)/profile/components/ProfileHeading.tsx
@@ -4,6 +4,7 @@ import { BiLogOut } from "react-icons/bi";
 import { Button } from "../../../components/ui/button";
 import React from "react";
 import { useUserContext } from "../../../../context/UserContext";
+import { poppins } from "../../../components";
 
 export const ProfileHeading = () => {
   const { user, logout } = useUserContext();
@@ -13,18 +14,17 @@ export const ProfileHeading = () => {
   };
 
   return (
-    <header className="flex justify-between p-6" data-testid="header">
+    <header className="flex justify-between" data-testid="header">
       <div>
-        <p className="font-semibold text-lg">{user?.fullName}</p>
-        <p>{user?.username}</p>
+        <p className={`${poppins.className} text-surface-light text-3xl font-bold`}>{user?.username}</p>
       </div>
       <Button
         data-testid="button"
         variant="filled"
-        className="bg-red-500 w-12 h-12"
+        className="bg-red-500 w-12 h-12 grid place-content-center"
         onClick={onLogout}
       >
-        <BiLogOut size={24} className="ml-2" />
+        <BiLogOut size={24} />
       </Button>
     </header>
   );
--- app/(site)/profile/page.tsx
+++ app/(site)/profile/page.tsx
@@ -1,10 +1,38 @@
 import React from "react";
 import { ProfileHeading } from "./components/ProfileHeading";
+import { poppins } from "../../components";
 
 export default function ProfilePage() {
   return (
-    <div data-testid="content">
+    <div data-testid="content" className="bg-secondary-dark p-6 min-h-screen">
       <ProfileHeading />
+
+      <section className="text-surface-light">
+        <h2 className={`font-bold ${poppins.className} text-2xl mb-2`}>Achievements</h2>
+        <div className="bg-secondary-light p-6 rounded-xl">
+          <p className={`font-bold ${poppins.className} text-lg`}>Disciplined Lifter</p>
+          <p className="text-surface-dark">You've completed all your monthly workouts</p>
+          <progress className="mt-4 h-16 w-full [&::-webkit-progress-bar]:rounded-xl [&::-webkit-progress-value]:rounded-xl [&::-webkit-progress-bar]:bg-secondary-dark [&::-webkit-progress-value]:bg-primary" max={12} value={4}> 4/12 </progress>
+        </div>
+      </section>
+
+      <section className="text-surface-light mt-6 flex flex-col gap-2">
+        <h2 className={`font-bold ${poppins.className} text-2xl`}>Settings</h2>
+        <div className="bg-secondary-light rounded-xl p-6 flex-1 flex justify-between">
+            <p className="text-xl text-surface-light">Language</p>
+            <p>&#62;</p>
+        </div>
+
+        <div className="bg-secondary-light rounded-xl p-6 flex-1 flex justify-between">
+            <p className="text-xl text-surface-light">Security</p>
+            <p>&#62;</p>
+          </div>
+
+          <div className="bg-secondary-light rounded-xl p-6 flex-1 flex justify-between">
+            <p className="text-xl text-surface-light">Notifications</p>
+            <p>&#62;</p>
+          </div>
+      </section>
     </div>
   );
 }
