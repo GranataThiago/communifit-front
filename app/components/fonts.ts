8e4b5a326db8b7da2af17c454308fbfdb2501664
mussiDev <joakoomussi@gmail.com>
2024-01-05 20:31:16 -0300

    delete duplicated code

--- app/components/fonts.ts
+++ app/components/fonts.ts
@@ -5,9 +5,3 @@ export const poppins = Poppins({
 	variable: "--font-poppins",
 	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
-
-export const poppins = Poppins({
-  subsets: ['latin'],
-  variable: "--font-poppins",
-  weight: ["600", "700", "800"]
-})
