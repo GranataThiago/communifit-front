8ae1e8680c6e8579bc20ef73d4e8073aa7b50980
mussiDev <joakoomussi@gmail.com>
2024-01-05 19:10:06 -0300

    change styles logo

--- app/components/Company/Logo.tsx
+++ app/components/Company/Logo.tsx
@@ -1,13 +1,13 @@
 const Logo = () => {
   return (
-    <div
-      role="heading"
-      aria-label="CommuniFit logo"
-      className="text-black text-4xl"
-    >
-      Communi<span className="text-primary">Fit</span>.
-    </div>
-  );
+		<h1
+			role='heading'
+			aria-label='CommuniFit logo'
+			className='text-primary text-4xl font-bold'
+		>
+			Communifit
+		</h1>
+	);
 };
 
 export default Logo;
