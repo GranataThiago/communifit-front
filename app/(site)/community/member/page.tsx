215811b5d882933e110ffaa8c665827db71ce443
mussiDev <joakoomussi@gmail.com>
2024-01-06 15:14:56 -0300

    add call api  list communities

--- app/(site)/community/member/page.tsx
+++ app/(site)/community/member/page.tsx
@@ -1,20 +1,41 @@
+import { IGetAllCommunity } from "../../../../interfaces/services/community/community-page";
 import { IMinimumUserInfo } from "../../../../interfaces/user";
 import { NonCommunityMemberScreen } from "../components/screens";
 import React from "react";
 import { cookies } from "next/headers";
 import { getAuthenticatedUser } from "../../../../helpers";
+import { getCommunities } from "../../../../services/community/community-page";
 import { redirect } from "next/navigation";
+
+const getListCommunities = async (
+	token: string
+): Promise<IGetAllCommunity[] | null> => {
+	try {
+		const response: IGetAllCommunity[] | null = await getCommunities({ token });
+
+		if (!response) return null;
+
+		return response;
+	} catch (error) {
+		console.error(error);
+		return null;
+	}
+};
 
 const CommunityPageMember = async () => {
 	const cookieStore = cookies();
-	const user: IMinimumUserInfo = await getAuthenticatedUser(
-		cookieStore.get("token")!.value
-	);
+	const token = cookieStore.get("token")!.value;
+	const user: IMinimumUserInfo = await getAuthenticatedUser(token);
 	if (!user) return redirect("/");
 	if (user.type !== "member") return redirect("/community");
+
+	const listCommunities: IGetAllCommunity[] | null =
+		await getListCommunities(token);
+
 	return (
 		<NonCommunityMemberScreen
 			community={user.community ? user.community : null}
+			communities={listCommunities}
 		/>
 	);
 };
