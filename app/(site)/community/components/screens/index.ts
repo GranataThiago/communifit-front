cbd508b14fe221daec59bf81dce1911d6665fff3
mussiDev <joakoomussi@gmail.com>
2024-01-06 13:34:38 -0300

    fix imports barrel

--- app/(site)/community/components/screens/index.ts
+++ app/(site)/community/components/screens/index.ts
@@ -1,4 +1,3 @@
 export { default as CommunityScreen } from "./CommunityScreen";
 export * from "./NonCommunityMemberScreen";
-export { default as NonCommunityScreen } from "./NonCommunityScreen";
 export * from "./NonCommunityTrainerScreen";
