3bd7c7acf949e534d169278af56edb22775bffc9
mussiDev <joakoomussi@gmail.com>
2024-01-06 12:26:31 -0300

    fix styles and data post

--- app/(site)/community/components/Post.tsx
+++ app/(site)/community/components/Post.tsx
@@ -1,33 +1,33 @@
-import Image from "next/image";
 import { BsPencil } from "react-icons/bs";
-import { format } from "date-fns";
 import { IPost } from "../../../../interfaces/community";
+import Image from "next/image";
+import { format } from "date-fns";
 
-export const Post = ({ fullname, username, body, datepublished }: IPost) => {
-  return (
-    <div className="flex flex-col gap-2">
-      <div className="flex justify-between items-center ">
-        <div className="flex gap-2">
-          <Image
-            className="rounded-full w-12 h-12"
-            src="https://i.pravatar.cc/300"
-            alt="fortys"
-            width={32}
-            height={32}
-          />
-          <p className="font-bold text-lg flex flex-col">
-            {fullname}
-            <span className="text-gray-400 text-sm font-medium">
-              {format(new Date(datepublished), "dd-MM")}
-            </span>
-          </p>
-        </div>
+export const Post = ({ fullname, body, datepublished }: IPost) => {
+	return (
+		<div className='flex flex-col gap-2 bg-secondary-light rounded-xl p-6 text-white'>
+			<div className='flex justify-between items-center '>
+				<div className='flex gap-2'>
+					<Image
+						className='rounded-full w-12 h-12'
+						src='https://i.pravatar.cc/300'
+						alt='fortys'
+						width={32}
+						height={32}
+					/>
+					<p className='font-bold text-lg flex flex-col'>
+						<span className='text-primary'>{fullname}</span>
+						<span className='text-sm font-medium'>
+							{format(new Date(datepublished), "dd-MM")}
+						</span>
+					</p>
+				</div>
 
-        <BsPencil className="text-gray-400"></BsPencil>
-      </div>
+				<BsPencil />
+			</div>
 
-      <p className="font-normal">{body}</p>
-      <p className="text-sm text-gray-400">Add comment</p>
-    </div>
-  );
+			<p className='font-normal'>{body}</p>
+			<p className='text-sm '>Add comment</p>
+		</div>
+	);
 };
