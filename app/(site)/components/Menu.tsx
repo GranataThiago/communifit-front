26eca6aabc29a2e2f82df37e7989f008ca637221
mussiDev <joakoomussi@gmail.com>
2024-01-06 18:44:25 -0300

    replace link to community

--- app/(site)/components/Menu.tsx
+++ app/(site)/components/Menu.tsx
@@ -1,14 +1,16 @@
 "use client";
+
+import {
+  BsChat,
+  BsHouseDoor,
+  BsPeople,
+  BsPerson,
+  BsSearch,
+} from "react-icons/bs";
+
+import Link from "next/link";
 import React from "react";
 import { usePathname } from "next/navigation";
-import Link from "next/link";
-import {
-  BsHouseDoor,
-  BsChat,
-  BsSearch,
-  BsPerson,
-  BsPeople,
-} from "react-icons/bs";
 import { useUserContext } from "../../../context/UserContext";
 
 export const Menu = () => {
@@ -16,54 +18,48 @@ export const Menu = () => {
   const { user } = useUserContext();
 
   return (
-    <nav
-      className="w-full py-4 px-6 fixed bottom-0 right-0 bg-secondary-light z-50"
-      data-testid="nav"
-    >
-      <ul className="w-full flex justify-between text-xl" data-testid="ul">
-        <li
-          className={`hover:text-gray-700 hover:cursor-pointer ${
-            pathname === "/" ? "text-primary" : "text-surface-light"
-          }`}
-        >
-          <Link href={"/"}>
-            <BsHouseDoor></BsHouseDoor>
-          </Link>
-        </li>
-        <li
-          className={`hover:text-gray-700 hover:cursor-pointer ${
-            pathname === "/chat" ? "text-primary" : "text-surface-light"
-          }`}
-        >
-          <Link href={"/chat"}>
-            <BsChat></BsChat>
-          </Link>
-        </li>
-        <li
-          className={`hover:text-gray-700 hover:cursor-pointer ${
-            pathname === "/community" ? "text-primary" : "text-surface-light"
-          }`}
-        >
-          <Link
-            href={
-              user?.community?.name
-                ? `/community/${user.community.name}`
-                : "/community"
-            }
-          >
-            {user?.community?.name ? <BsPeople /> : <BsSearch />}
-          </Link>
-        </li>
-        <li
-          className={`hover:text-gray-700 hover:cursor-pointer ${
-            pathname === "/profile" ? "text-primary" : "text-surface-light"
-          }`}
-        >
-          <Link href={"/profile"}>
-            <BsPerson></BsPerson>
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  );
+		<nav
+			className='w-full py-4 px-6 fixed bottom-0 right-0 bg-secondary-light z-50'
+			data-testid='nav'
+		>
+			<ul className='w-full flex justify-between text-xl' data-testid='ul'>
+				<li
+					className={`hover:text-gray-700 hover:cursor-pointer ${
+						pathname === "/" ? "text-primary" : "text-surface-light"
+					}`}
+				>
+					<Link href={"/"}>
+						<BsHouseDoor></BsHouseDoor>
+					</Link>
+				</li>
+				<li
+					className={`hover:text-gray-700 hover:cursor-pointer ${
+						pathname === "/chat" ? "text-primary" : "text-surface-light"
+					}`}
+				>
+					<Link href={"/chat"}>
+						<BsChat></BsChat>
+					</Link>
+				</li>
+				<li
+					className={`hover:text-gray-700 hover:cursor-pointer ${
+						pathname === "/community" ? "text-primary" : "text-surface-light"
+					}`}
+				>
+					<Link href={"/community"}>
+						{user?.community?.name ? <BsPeople /> : <BsSearch />}
+					</Link>
+				</li>
+				<li
+					className={`hover:text-gray-700 hover:cursor-pointer ${
+						pathname === "/profile" ? "text-primary" : "text-surface-light"
+					}`}
+				>
+					<Link href={"/profile"}>
+						<BsPerson></BsPerson>
+					</Link>
+				</li>
+			</ul>
+		</nav>
+	);
 };
