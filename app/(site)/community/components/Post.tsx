639c663d77672742729a3dd84c5ad85479ab3680
mussiDev <joakoomussi@gmail.com>
2024-01-10 20:32:21 -0300

    fix post page

--- app/(site)/community/components/Post.tsx
+++ app/(site)/community/components/Post.tsx
@@ -1,9 +1,18 @@
+"use client";
+
+import { AiOutlineClose } from "react-icons/ai";
 import { BsPencil } from "react-icons/bs";
+import { Button } from "../../../components/ui/button";
 import { IPost } from "../../../../interfaces/community";
 import Image from "next/image";
+import { Input } from "../../../components/ui/input";
 import { format } from "date-fns";
+import { useState } from "react";
 
 export const Post = ({ fullname, body, datepublished }: IPost) => {
+	const [addComent, setAddComment] = useState(false);
+	const [edit, setEdit] = useState(false);
+
 	return (
 		<div className='flex flex-col gap-2 bg-secondary-light rounded-xl p-6 text-white'>
 			<div className='flex justify-between items-center '>
@@ -23,11 +32,48 @@ export const Post = ({ fullname, body, datepublished }: IPost) => {
 					</p>
 				</div>
 
-				<BsPencil />
+				{edit ? (
+					<AiOutlineClose
+						className='text-gray-400 cursor-pointer'
+						role='button'
+						tabIndex={0}
+						data-testid='cancel-edit'
+						onClick={() => setEdit(false)}
+					/>
+				) : (
+					<BsPencil
+						className='text-gray-400 cursor-pointer'
+						role='button'
+						tabIndex={0}
+						data-testid='edit-post'
+						onClick={() => setEdit(true)}
+					/>
+				)}
 			</div>
 
-			<p className='font-normal'>{body}</p>
-			<p className='text-sm '>Add comment</p>
+			{edit ? (
+				<Input variant='outlined' defaultValue={body} />
+			) : (
+				<p className='font-normal w-[80%]'>{body}</p>
+			)}
+
+			<Button
+				variant='text'
+				className='text-sm text-gray-400 justify-start w-24'
+				onClick={() => setAddComment(!addComent)}
+				data-testid='add-comment'
+				role='button'
+			>
+				Add comment
+			</Button>
+			{addComent && (
+				<Input
+					variant='outlined'
+					data-testid='add-comment-input'
+					className='focus-visible:ring-none'
+					placeholder='Write your comment here...'
+				/>
+			)}
 		</div>
 	);
 };
