fcbc5c1708c106a9be58e2b269dc7fc3933f987c
mussiDev <joakoomussi@gmail.com>
2024-01-11 20:40:12 -0300

    pass props and create input double

--- app/(site)/community/components/CreateCommunityForm.tsx
+++ app/(site)/community/components/CreateCommunityForm.tsx
@@ -16,17 +16,30 @@ import {
 import { AiFillCloseCircle } from "react-icons/ai";
 import { Button } from "../../../components/ui/button";
 import { CreateCommunity } from "../../../../interfaces/services/community/create-community";
+import { ICommunity } from "../../../../interfaces";
 import { Input } from "../../../components/ui/input";
 import { montserrat } from "../../../components/fonts";
 import { renderToast } from "../../../providers/ToasterProvider";
 import { useCookies } from "react-cookie";
 import { useUserContext } from "../../../../context/UserContext";
 
-const CreateCommunityForm = ({ isEdit }: { isEdit?: boolean }) => {
+const CreateCommunityForm = ({
+	isEdit,
+	community,
+}: {
+	isEdit?: boolean;
+	community?: ICommunity;
+}) => {
 	const [cookies] = useCookies(["token"]);
 	const { user } = useUserContext();
 	const { control, watch, setValue, formState, handleSubmit } =
-		useForm<CreateCommunity>();
+		useForm<CreateCommunity>({
+			defaultValues: {
+				displayName: community?.displayname ?? "",
+				name: community?.name ?? "",
+				description: community?.description ?? "",
+			},
+		});
 
 	const displayName = watch("displayName");
 
@@ -55,11 +68,16 @@ const CreateCommunityForm = ({ isEdit }: { isEdit?: boolean }) => {
 		// Ideally back would return the createdCommunity in the response object
 		window.location.href = `/community/${formData.name?.toLowerCase()}`;
 	};
-
+	const handleDivClick = () => {
+		const input = document.getElementById("url");
+		if (input) {
+			input.focus();
+		}
+	};
 	return (
 		<section>
 			<form
-				className={`flex flex-col items-center justify-center gap-6 ${montserrat.className} text-white`}
+				className={`flex flex-col mx-20 justify-center gap-6 ${montserrat.className} text-white`}
 				onSubmit={handleSubmit(onCommunityCreated)}
 			>
 				<h1 className='text-2xl font-bold mt-4'>
@@ -88,42 +106,35 @@ const CreateCommunityForm = ({ isEdit }: { isEdit?: boolean }) => {
 					)}
 				/>
 
-				<div className='flex flex-col'>
-					<div className='flex items-center justify-center w-80 md:w-100'>
+				<FormField
+					rules={{
+						required: "The Name is required.",
+					}}
+					control={control}
+					name='name'
+					render={({ field }) => (
 						<FormItem>
-							<FormLabel className='mb-2'>Community URL</FormLabel>
-							<Input
-								value={"www.communifit.com/"}
-								ref={null}
-								variant='outlined'
-								type='text'
-								className='text-black'
-							/>
+							<FormLabel>Community URL</FormLabel>
+							<FormControl>
+								<div
+									onClick={handleDivClick}
+									role='input'
+									className='flex bg-white text-black items-center rounded-md'
+								>
+									<p className='text-sm pl-2'>www.communifit.com/</p>
+									<Input
+										id='url'
+										{...field}
+										ref={null}
+										variant='outlined'
+										type='text'
+										className='text-black border-0 overflow-hidden p-0 focus-visible:ring-offset-0 focus-visible:ring-0'
+									/>
+								</div>
+							</FormControl>
 						</FormItem>
-
-						<FormField
-							rules={{
-								required: "The Name is required.",
-							}}
-							control={control}
-							name='name'
-							render={({ field }) => (
-								<FormItem className='w-32 md:w-100'>
-									<FormLabel>Name</FormLabel>
-									<FormControl>
-										<Input
-											{...field}
-											ref={null}
-											variant='outlined'
-											type='text'
-											className='text-black'
-										/>
-									</FormControl>
-								</FormItem>
-							)}
-						/>
-					</div>
-				</div>
+					)}
+				/>
 				<FormField
 					rules={{
 						required: "The Description is required.",
@@ -146,7 +157,11 @@ const CreateCommunityForm = ({ isEdit }: { isEdit?: boolean }) => {
 					)}
 				/>
 
-				<Button className='mt-2 w-48' variant='filled' type='submit'>
+				<Button
+					variant='customize'
+					className='bg-primary text-secondary py-3 text-md'
+					type='submit'
+				>
 					{isEdit ? "Edit" : "Create"}
 				</Button>
 			</form>
